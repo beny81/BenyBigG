5850b633107766df4cd28413c09537e8858a50d4
beny81 <152813531+beny81@users.noreply.github.com>
2024-02-03 10:22:40 -0500

    Add files via upload

---
+++ site/script.js
@@ -0,0 +1,65 @@
+document.addEventListener("DOMContentLoaded", 
+      function (event){ 
+              
+        
+              // document.querySelector("#btt")
+              //         .addEventListener("blur", function(event){
+                          
+        
+                         
+          
+                      
+              //           let screenWidth = window.innerWidth;
+                         
+                     
+              //                     if(screenWidth<769 ){
+                         
+              //                      document.querySelector("#btt") .click();
+                                      
+                                  
+                                      
+                                     
+                                     
+              //                         }
+
+                          
+                              
+                              
+                         
+              //             }); 
+           
+
+                 document.querySelector("#bell")
+                      .addEventListener("click",()=> {  
+
+                    Notification.requestPermission().then (perm =>{
+                        // alert("hello")
+                        if(perm=== "granted"){
+                           new Notification("hello");
+                           console.log("hello");
+                          new Notification("Hello",{      
+                          
+                          body:"Have a great day"
+                          
+                          
+                          })
+                            
+
+
+                            }
+                          
+
+                           })
+                      })
+
+         
+
+                                                                
+                 
+                              
+       });
+// var showLoading = function (selector) {
+//   var html = "<div class='text-center'>";
+//   html += "<img src='images/ajax-loader.gif'></div>";
+//   insertHtml(selector, html);
+// };
